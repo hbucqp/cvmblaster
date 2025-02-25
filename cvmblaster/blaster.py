0054aafae134d2b2ebf0d6ac73b5c685e58d206b
崔箐坡 <hbucqp1991@sina.cn>
2025-02-25 19:02:46 +0800

    add mlst blast function

--- cvmblaster/blaster.py
+++ cvmblaster/blaster.py
@@ -369,6 +369,83 @@ class Blaster():
         # print(hsp_results)
         return df, hsp_results
 
+    def mlst_blast(self):
+        cline = [self.blast_type, '-query', self.inputfile, '-db', self.database, '-dust', 'no', '-ungapped'
+                 '-evalue', '1E-20', '-out', self.temp_output,
+                 '-outfmt', '"6 sseqid slen length nident"', '-perc_identity', self.minid, '-max_target_seqs', '1000000',
+                 '-num_threads', str(self.threads)]
+
+        # cline = NcbiblastnCommandline(query=self.inputfile, db=self.database, dust='no', ungapped=True,
+        #                               evalue=1E-20, out=self.temp_output,  # delete culling_limit parameters
+        #                               outfmt='"6 sseqid slen length nident"',
+        #                               # task='dc-megablast',
+        #                               perc_identity=self.minid, max_target_seqs=1000000,
+        #                               num_threads=self.threads)
+        # print(cline)
+        stdout, stderr = cline()
+        df = pd.read_csv(self.temp_output, sep='\t', names=[
+            'sseqid', 'slen', 'length', 'nident'])
+        # print(df)
+
+        result = {}
+        for i, row in df.iterrows():
+            gene, num = re.match(
+                '^(\w+)[_-](\d+)', row['sseqid']).group(1, 2)
+            # print(gene)
+            num = int(num)
+            hlen = row['slen']
+            alen = row['length']
+            nident = row['nident']
+            if nident * 100 / hlen >= self.mincov:
+                # if sch not in result.keys():  # check if sch is the key of result
+                #     result[sch] = {}
+                # resolve the bug that could not get exactly matched allele
+                if hlen == alen & nident == hlen:  # exact match
+                    if gene in result.keys():
+
+                        if not re.search(r'[~\?]', str(result[gene])):
+                            old_num = int(result[gene])
+                            if num < old_num:
+                                # print(f'{num}\t{old_num}')
+                                result[gene] = num
+                                print(
+                                    f'Found additional allele match, replace {gene}:{old_num} -> {num}')
+                            else:
+                                print(
+                                    f'Found additional allele match, but the allele number {num} is greater or equal to stored one {gene}:{old_num}, skip...')
+                        else:  # replace not perfect match
+                            result[gene] = num
+                    else:
+                        result[gene] = num
+                # new allele
+                elif (alen == hlen) & (nident != hlen):
+                    # print('xx')
+                    if gene not in result.keys():
+                        # print('xxx')
+                        result[gene] = f'~{num}'
+                    else:
+                        next
+                    # result[sch] = mlst
+                elif (alen != hlen) & (nident == hlen):  # partial match
+                    if gene not in result.keys():
+                        result[gene] = f'{num}?'
+                else:
+                    next
+        # remove temp blastn output file
+        os.remove(self.temp_output)
+        return result
+
+    @staticmethod
+    def makeblastdb(file):
+        cline = NcbimakeblastdbCommandline(
+            dbtype="nucl", title='cgMLST',
+            hash_index=True,
+            parse_seqids=True,
+            input_file=file)
+        print(f"Making database...")
+        stdout, stderr = cline()
+        print(stdout)
+
     @ staticmethod
     def filter_results(save, best_result, tmp_results):
         """
